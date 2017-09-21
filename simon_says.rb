849222abf64786b8c149d55cc33845b27a0fbf80
KaiserChan <chankaiserkk@gmail.com>
2017-09-20 21:02:52 -0400

    updated reinforcement_tdd_simon ... added some comments

--- simon_says.rb
+++ simon_says.rb
@@ -15,9 +15,9 @@ def repeat(phrase, repeat_times)
 end
 
 def start_of_word(word, num_of_letters_display)
-  word[0,num_of_letters_display]
+  word[0,num_of_letters_display] #slice ... split up a string into its individual letters ... 1st input (i.e. 0) is starting index position
 end
 
 def first_word(phrase)
-  phrase.partition(" ").first
+  phrase.partition(" ").first #partition ... split up a string based on the input ... in this case, the space between the words of a phrase argument
 end
